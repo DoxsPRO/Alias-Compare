272723424a95133a521949f9ce25cb756491c572
DoxsPRO <domenico.ciancio@studio.unibo.it>
2024-04-08 09:22:34 +0200

    added image and textbox

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
+import React from "react";
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import { Textarea } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Alias-Compare</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -19,34 +22,18 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <Image src="/images/prova.jpg" alt="test" width={300} height={300}></Image>
+          </div>
+          <div className={styles.card}>
+            <Textarea
+              label="Testo"
+              placeholder="Enter your description"
+              className="max-w-xs"
+            />
+          </div>
         </div>
+
       </main>
 
       <footer>
