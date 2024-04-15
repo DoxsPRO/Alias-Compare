a388d3711702c9cc959b5872b6bfe6a3b6bccb66
DoxsPRO <domenico.ciancio@studio.unibo.it>
2024-04-15 15:51:52 +0200

    "Added extraction of names and dates from text; updated nomiCognomiArray to use let; fixed off-by-one error in line indexing"

--- app/json.js
+++ app/json.js
@@ -6,18 +6,20 @@ function separaParoleAttaccate(stringa) {
 const riconosciNome = (testo) => {
   //console.log(testo);
   var datiPersona = {};
-  const regexNome = /sotto\s*([^,;]+)/g;
-  const regexCognomeNome = /([A-Z]+(?:\s+[A-Z]+)*)\s+([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*)|([A-Z]+[a-z]+)\s*([A-Z][a-z]+)/;
-  const linee = testo.split('\n');
   let nomiCognomiArray = []; // Dichiarazione dell'array per salvare nomi e cognomi
 
+  const regexNome = /sotto\s*([^,;]+)/g;
+  const regexCognomeNome = /([A-Z]+(?:\s+[A-Z]+)*)\s+([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*)|([A-Z]+[a-z]+)\s*([A-Z][a-z]+)/;
+  const regexData = /nato\s*([^,;]+)/g;
+
+  const linee = testo.split('\n');
 
   linee.forEach((linea, index) => {
     //if (!(linea.startsWith('|') || linea.startsWith('*'))) {
     //console.log(linea);
     if (linea.match(regexNome)) {
-      if (linee[index + 1] && linee[index + 2]) { //controlla se la linea succ esiste
-        datiPersona = linea + linee[index + 1] + linee[index + 2];
+      if (linee[index + 1] && linee[index + 2] && linee[index + 3]) { //controlla se la linea succ esiste
+        datiPersona = linea + linee[index + 1] + linee[index + 2] + linee[index + 3];
         datiPersona.replace(/\n/g, ' ');
         datiPersona = separaParoleAttaccate(datiPersona);
         console.log(datiPersona);
@@ -25,6 +27,10 @@ const riconosciNome = (testo) => {
         if (matchNome) {
           console.warn(matchNome[1] + " " + matchNome[2]);
           nomiCognomiArray.push(`${matchNome[1]} ${matchNome[2]}`);
+        }
+        var matchData = datiPersona.match(regexData);
+        if(matchData) {
+          console.info(matchData);
         }
       }
 
